58bc770961fd3b6aa21568ebc6f62f5766326bdd
Ono-Tenshi <tenshi41922960@icloud.com>
2026-02-15 20:27:44 +0900

    Add files via upload

---
+++ 2026_02_08 TODO/app.js
@@ -0,0 +1,79 @@
+const input = document.getElementById("todoInput");
+const addBtn = document.getElementById("addBtn");
+const list = document.getElementById("todoList");
+
+function updateSummary() {
+    const total = list.children.length; // liの総数
+    const done = list.querySelectorAll("span.done").length; // 完了済みの数
+    const notDone = total - done; // 未完了の数
+
+    document.getElementById("summary").textContent = `合計:${total} 完了:${done} 未完了:${notDone}`;
+}
+
+function saveTodos() {
+    const todos = [];
+    list.querySelectorAll("li").forEach(li => {
+        const span = li.querySelector("span");
+        todos.push({ text: span.textContent, done: span.classList.contains("done") });
+    });
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function loadTodos() {
+    // 保存していたタスクを取得
+    const todos = JSON.parse(localStorage.getItem("todos")) || [];
+    
+    // 配列を順番に処理して画面に表示
+    todos.forEach(todo => {
+        // createTodoElement に文字と完了状態を渡す
+        createTodoElement(todo.text, todo.done);
+    });
+
+    // 日数表示も更新
+    updateSummary();
+}
+
+window.addEventListener("load", loadTodos);
+
+function createTodoElement(text, doneState = false) {
+    const li = document.createElement("li");
+
+    const span = document.createElement("span");
+    span.textContent = text;
+    if (doneState) {
+        span.classList.add("done");
+        span.style.textDecoration = "line-through";
+    }
+
+    const doneBtn = document.createElement("button");
+    doneBtn.textContent = "完了";
+    doneBtn.addEventListener("click", function() {
+        span.classList.toggle("done");
+        span.style.textDecoration = span.classList.contains("done") ? "line-through" : "none";
+        updateSummary();
+        saveTodos();
+    });
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "削除";
+    deleteBtn.addEventListener("click", function() {
+        list.removeChild(li);
+        updateSummary();
+        saveTodos();
+    });
+
+    li.appendChild(span);
+    li.appendChild(doneBtn);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+}
+
+addBtn.addEventListener("click", function () {
+    const text = input.value.trim();
+    if (!text) return;
+
+    createTodoElement(text);
+    updateSummary();
+    saveTodos();
+    input.value = "";
+});
